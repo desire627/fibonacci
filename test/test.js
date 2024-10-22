abae430a550add8326f0d588fdf2db094d49e4db
desire627 <desireasiimwe24@gmail.com>
2024-10-22 20:14:04 +0300

    add this

--- test/test.js
+++ test/test.js
@@ -5,4 +5,9 @@ describe('Fibonacci function', () => {
   it('should return 0 for fibonacci(0)', () => {
     assert.equal(fibonacci(0), 0);
   });
+
+
+it('should return 1 for fibonacci(1)', () => {
+  assert.equal(fibonacci(1), 1);
+   });
 });
